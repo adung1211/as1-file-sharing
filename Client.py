d98ede7ecad3d064b336cc06863f6b74ece7e1d3
adung1211 <57287981+adung1211@users.noreply.github.com>
2023-11-30 10:16:59 +0700

    Add files via upload

--- Client.py
+++ Client.py
@@ -4,7 +4,7 @@ import threading
 import tqdm
 import json
 
-SEVER_LOCAL_IP = '192.168.205.132'
+SERVER_LOCAL_IP = '127.0.0.1'
 
 
 class Client:
@@ -16,13 +16,14 @@ class Client:
 
         self.p2p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.p2p_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.p2p_socket.bind((self.client_socket.getsockname()[0], self.client_socket.getsockname()[1]))
+        self.p2p_socket.bind((self.client_socket.getsockname()[
+                             0], self.client_socket.getsockname()[1]))
         self.p2p_socket.listen(5)
 
     def fetch(self, filename):
         data = self.client_socket.recv(1024)
         source = json.loads(data.decode())
-        
+
         if (len(source) == 0):
             print("None hostname published that file you requested")
             return
@@ -30,7 +31,7 @@ class Client:
         for source_i in source:
             print(source_i)
         print("Choose the address and port of the hostname you want to fetch form")
-        
+
         while True:
             try:
                 source_addr = input(">Address: ")
@@ -43,16 +44,19 @@ class Client:
             except ConnectionRefusedError:
                 print("Connection fail, please try another hostname")
                 continue
-    
+
     def publish(self, fname):
         data = self.client_socket.recv(1024)
         if data == b"OK":
-            print("Publish success file {}".format(fname)) 
+            print("Publish success file {}".format(fname))
 
     def send_command(self):
         print("To start, type 'publish fname' or 'fetch fname' or 'quit'")
         while True:
             command = input("> ")
+
+            if not command:
+                continue
 
             type, *arg = command.split()
             if type == 'fetch':
@@ -63,21 +67,24 @@ class Client:
                 self.publish(arg[0])
             elif type == 'quit':
                 os._exit(0)
+            else:
+                print("Not valid commad !")
 
     def p2p_receive(self, filename, down_soc):
         msg = "Request " + str(filename)
-        down_soc.send(str(msg).encode()) 
-        
-        #file_down = down_soc.recv(1024).decode()
+        down_soc.send(str(msg).encode())
+
+        # file_down = down_soc.recv(1024).decode()
         file_down = "received_" + filename
         print("Downloading {}...".format(filename))
         file_size = down_soc.recv(1024).decode()
-        #print("{} bytes".format(file_size))
+        # print("{} bytes".format(file_size))
 
         file = open(file_down, "wb")
         file_bytes = b""
         done = False
-        progress =tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(file_size))
+        progress = tqdm.tqdm(unit="B", unit_scale=True,
+                             unit_divisor=1000, total=int(file_size))
 
         while not done:
             data = down_soc.recv(128 * 1024)
@@ -86,7 +93,7 @@ class Client:
             else:
                 file_bytes += data
             progress.update(128 * 1024)
-        
+
         file.write(file_bytes)
         file.close()
         print("\nDownload {} completed".format(filename))
@@ -95,17 +102,17 @@ class Client:
         file = open(filename, "rb")
         file_size = os.path.getsize(filename)
 
-        #send_socket.send(str("received_" + filename).encode())
+        # send_socket.send(str("received_" + filename).encode())
         send_socket.send(str(file_size).encode())
 
         data = file.read()
         send_socket.sendall(data)
         send_socket.send(b"<END>")
         file.close()
-        send_socket.close() #!!!!!!!!!!!!!!!!!!!
+        send_socket.close()  # !!!!!!!!!!!!!!!!!!!
 
     def p2p_handle(self, send_socket, send_addr):
-        #print("P2P connected to {}".format(send_addr))
+        # print("P2P connected to {}".format(send_addr))
         data = send_socket.recv(1024).decode('utf-8')
         type, *args = data.split()
         if type == 'Request':
@@ -116,8 +123,9 @@ class Client:
     def p2p_connection(self):
         while True:
             send_socket, send_addr = self.p2p_socket.accept()
-            #print("Connected p2p to {}".format(send_addr))
-            threading.Thread(target=self.p2p_handle, args=(send_socket, send_addr)).start()
+            # print("Connected p2p to {}".format(send_addr))
+            threading.Thread(target=self.p2p_handle, args=(
+                send_socket, send_addr)).start()
 
     def start(self):
         print("Connected to File-sharing server")
@@ -133,13 +141,12 @@ class Client:
                 print("Hostname is already running, please retry !")
                 continue
             break
-            
-        
+
         threading.Thread(target=self.p2p_connection, args=()).start()
-        #self.p2p_connection()
+        # self.p2p_connection()
         self.send_command()
+
 
 if __name__ == "__main__":
     client = Client((SERVER_LOCAL_IP, 8080))
     client.start()
-    
--- Server.py
+++ Server.py
@@ -2,7 +2,8 @@ import socket
 import threading
 import json
 
-LOCAL_IP = '192.168.205.132'
+LOCAL_IP = '127.0.0.1'
+
 
 class Server:
     def __init__(self):
@@ -12,20 +13,21 @@ class Server:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((LOCAL_IP, 8080))
         self.server_socket.listen(5)
-    
+
     def fetch(self, client_socket, fname):
         source = []
         for hostname in self.clients:
             if fname in self.clients[hostname]:
-                source.append("Hostname: " + hostname + ", Address: " + str(self.host_addr[hostname][0]) + ", Port: " + str(self.host_addr[hostname][1]))
+                source.append("Hostname: " + hostname + ", Address: " + str(
+                    self.host_addr[hostname][0]) + ", Port: " + str(self.host_addr[hostname][1]))
         client_socket.sendall(json.dumps(source).encode())
-        #client_socket.send(b"<END>")
-    
-    def publish(self,client_socket, hostname, fname):
+        # client_socket.send(b"<END>")
+
+    def publish(self, client_socket, hostname, fname):
         self.clients[hostname].append(fname)
         print("Success publish " + fname + " from {}".format(hostname))
         client_socket.send(b"OK")
-        
+
     def handle_client(self, client_socket, client_addr):
         hostname = self.login(client_socket, client_addr)
         try:
@@ -43,8 +45,8 @@ class Server:
                 elif type == 'quit':
                     break
         except ConnectionResetError:
-                print("Disconnected to {}".format(hostname))
-    
+            print("Disconnected to {}".format(hostname))
+
     def discover(self, hostname):
         for hostname_i in self.clients:
             if hostname_i == hostname:
@@ -54,21 +56,22 @@ class Server:
                     print(fname)
                 print("Success discover")
                 return
-        #Can not find that hostname
+        # Can not find that hostname
         print("Error: Not existed that hostname")
-    
+
     def ping(self, hostname):
         if hostname in self.clients:
             client_addr = self.host_addr[hostname]
-            self.temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #self.temp_socket.settimeout(3)
+            self.temp_socket = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+            # self.temp_socket.settimeout(3)
             try:
                 self.temp_socket.connect(client_addr)
                 self.temp_socket.send(b"PING")
             except ConnectionRefusedError:
-                    print("{} is not active".format(hostname))
-                    self.temp_socket.close()
-                    return False
+                print("{} is not active".format(hostname))
+                self.temp_socket.close()
+                return False
             else:
                 print("{} is active".format(hostname))
                 self.temp_socket.close()
@@ -77,15 +80,20 @@ class Server:
             print(hostname + " not exist !")
             self.temp_socket.close()
         return False
-    
+
     def start_shell(self):
         while True:
             command = input()
+            if not command:
+                continue
+
             type, *arg = command.split()
             if type == 'discover':
                 self.discover(arg[0])
             elif type == 'ping':
                 self.ping(arg[0])
+            else:
+                print("Not valid commad !")
 
     def login(self, client_socket, client_addr):
         while True:
@@ -95,21 +103,23 @@ class Server:
                 self.host_addr[hostname] = client_addr
                 client_socket.send(b"REG")
             else:
-                if  self.ping(hostname) == True:
+                if self.ping(hostname) == True:
                     client_socket.send(b"FAIL")
                     continue
                 else:
                     self.host_addr[hostname] = client_addr
                     client_socket.send(b"LOG")
             return hostname
-    
+
     def start(self):
         print("Server listening for incoming connections...")
         threading.Thread(target=self.start_shell, args=()).start()
         while True:
-            client_socket, client_addr = self.server_socket.accept()    
+            client_socket, client_addr = self.server_socket.accept()
             print("Connected to {}".format(client_addr))
-            threading.Thread(target=self.handle_client, args=(client_socket, client_addr)).start()
+            threading.Thread(target=self.handle_client, args=(
+                client_socket, client_addr)).start()
+
 
 if __name__ == "__main__":
     server = Server()
